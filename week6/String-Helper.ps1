f77c2795e23a8074de4facb9c1b782451ef55f9b
skyleroriordan <skyler.oriordan@mymail.champlain.edu>
2025-02-25 14:53:27 -0500

    new file:   Event-Logs.ps1 	new file:   String-Helper.ps1 	new file:   Users.ps1 	new file:   main-1.ps1

---
+++ week6/String-Helper.ps1
@@ -0,0 +1,54 @@
+﻿<# String-Helper
+*************************************************************
+   This script contains functions that help with String/Match/Search
+   operations. 
+************************************************************* 
+#>
+
+
+<# ******************************************************
+   Functions: Get Matching Lines
+   Input:   1) Text with multiple lines  
+            2) Keyword
+   Output:  1) Array of lines that contain the keyword
+********************************************************* #>
+function getMatchingLines($contents, $lookline){
+
+$allines = @()
+$splitted =  $contents.split([Environment]::NewLine)
+
+for($j=0; $j -lt $splitted.Count; $j++){  
+ 
+   if($splitted[$j].Length -gt 0){  
+        if($splitted[$j] -ilike $lookline){ $allines += $splitted[$j] }
+   }
+
+}
+
+return $allines
+}
+
+function checkPassword($password) {
+    $passwordText = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto(
+        [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($password))
+   
+   if($passwordText.Length -lt 6){ 
+        return $false
+    }
+
+    if($passwordText -notmatch "[A-Za-z]"){
+    return $false
+    }
+
+    if($passwordText -notmatch "\d"){
+    return $false
+    }
+
+    if($passwordText -notmatch "[^A-Za-z0-9]"){
+    return $false
+    }
+
+    else {
+    return $true
+    }
+}
---
+++ week6/main-1.ps1
@@ -0,0 +1,207 @@
+﻿. (Join-Path $PSScriptRoot Users.ps1)
+. (Join-Path $PSScriptRoot Event-Logs.ps1)
+
+clear
+
+$Prompt = "`n"
+$Prompt += "Please choose your operation:`n"
+$Prompt += "1 - List Enabled Users`n"
+$Prompt += "2 - List Disabled Users`n"
+$Prompt += "3 - Create a User`n"
+$Prompt += "4 - Remove a User`n"
+$Prompt += "5 - Enable a User`n"
+$Prompt += "6 - Disable a User`n"
+$Prompt += "7 - Get Log-In Logs`n"
+$Prompt += "8 - Get Failed Log-In Logs`n"
+$Prompt += "9 - Exit`n"
+$Prompt += "10 - List at risk Users`n"
+
+
+
+$operation = $true
+
+while($operation){
+
+    
+    Write-Host $Prompt | Out-String
+    $choice = Read-Host 
+
+
+    if($choice -eq 9){
+        Write-Host "Goodbye" | Out-String
+        exit
+        $operation = $false 
+    }
+
+    elseif($choice -eq 1){
+        $enabledUsers = getEnabledUsers
+        Write-Host ($enabledUsers | Format-Table | Out-String)
+    }
+
+    elseif($choice -eq 2){
+        $notEnabledUsers = getNotEnabledUsers
+        Write-Host ($notEnabledUsers | Format-Table | Out-String)
+    }
+
+
+    # Create a user
+    elseif($choice -eq 3){ 
+
+        $name = Read-Host -Prompt "Please enter the username for the new user"
+        if (checkUser $name) {
+            Write-Host "User already exists"
+            continue
+        }
+
+        $password = Read-Host -AsSecureString -Prompt "Please enter the password for the new user"
+
+        # TODO: Create a function called checkUser in Users that: 
+        #              - Checks if user a exists. 
+        #              - If user exists, returns true, else returns false
+        # TODO: Check the given username with your new function.
+        #              - If false is returned, continue with the rest of the function
+        #              - If true is returned, do not continue and inform the user
+        #
+        # TODO: Create a function called checkPassword in String-Helper that:
+        #              - Checks if the given string is at least 6 characters
+        #              - Checks if the given string contains at least 1 special character, 1 number, and 1 letter
+        #              - If the given string does not satisfy conditions, returns false
+        #              - If the given string satisfy the conditions, returns true
+        # TODO: Check the given password with your new function. 
+        #              - If false is returned, do not continue and inform the user
+        #              - If true is returned, continue with the rest of the function  
+        if (-not (checkPassword $password)){
+        Write-Host "The password does not meet params"
+        continue
+        }
+         
+        createAUser $name $password
+
+        Write-Host "User: $name is created." | Out-String
+    }
+
+
+    # Remove a user
+    elseif($choice -eq 4){
+
+        $name = Read-Host -Prompt "Please enter the username for the user to be removed"
+
+        # TODO: Check the given username with the checkUser function.
+
+        if(-not (checkUser $name)){
+        Write-Host "User does not exist"
+        continue
+        }
+
+        removeAUser $name
+
+        Write-Host "User: $name Removed." | Out-String
+    }
+
+
+    # Enable a user
+    elseif($choice -eq 5){
+
+
+        $name = Read-Host -Prompt "Please enter the username for the user to be enabled"
+
+        # TODO: Check the given username with the checkUser function.
+
+        if(-not (checkUser $name)){
+        Write-Host "User does not exist"
+        continue
+        }
+
+        enableAUser $name
+
+        Write-Host "User: $name Enabled." | Out-String
+    }
+
+
+    # Disable a user
+    elseif($choice -eq 6){
+
+        $name = Read-Host -Prompt "Please enter the username for the user to be disabled"
+
+        if(-not (checkUser $name)){
+        Write-Host "User does not exist"
+        continue
+        }
+
+        # TODO: Check the given username with the checkUser function.
+
+        disableAUser $name
+
+        Write-Host "User: $name Disabled." | Out-String
+    }
+
+
+    elseif($choice -eq 7){
+
+        $name = Read-Host -Prompt "Please enter the username for the user logs"
+        
+        if(-not (checkUser $name)){
+        Write-Host "User does not exist"
+        continue
+        }
+        # TODO: Check the given username with the checkUser function.
+
+        $daysBack = Read-Host -Prompt "How many dayback do you want logs for?"
+        $userLogins = getLogInAndOffs $daysBack
+        # TODO: Change the above line in a way that, the days 90 should be taken from the user
+
+        Write-Host ($userLogins | Where-Object { $_.User -ilike "*$name"} | Format-Table | Out-String)
+    }
+
+
+    elseif($choice -eq 8){
+
+        $name = Read-Host -Prompt "Please enter the username for the user's failed login logs"
+
+        # TODO: Check the given username with the checkUser function.
+
+        if(-not (checkUser $name)){
+        Write-Host "User does not exist"
+        continue
+        }
+
+        $daysBack = Read-Host -Prompt "How many days back do you want logs for?"
+        $userLogins = getLogInAndOffs $daysBack
+        # TODO: Change the above line in a way that, the days 90 should be taken from the user
+
+        Write-Host ($userLogins | Where-Object { $_.User -ilike "*$name"} | Format-Table | Out-String)
+    }
+
+    elseif($choice -eq 10){
+        $daysBack = Read-Host -Prompt "How many days back do you want failed logins for?"
+        $failedlogins = getFailedLogins $daysBack
+        $atriskusers =  $failedlogins | Group-Object -Property User -NoElement | Where-Object { $_.Count -gt 10 } 
+
+        if($atriskusers){
+        Write-Host "These users had more than 10 failed logins:"
+        foreach($entry in $atriskusers){
+        Write-Host ("{0} => {1} failed logins" -f $entry.Name, $entry.Count)
+        }
+        }
+        else {
+        Write-Host "No users had more than 10 failed logins"
+        }
+    }
+
+    else {
+        Write-Host "Invalid choice"
+    }
+    # TODO: Create another choice "List at Risk Users" that
+    #              - Lists all the users with more than 10 failed logins in the last <User Given> days.  
+    #                (You might need to create some failed logins to test)
+    #              - Do not forget to update prompt and option numbers
+    
+    # TODO: If user enters anything other than listed choices, e.g. a number that is not in the menu   
+    #       or a character that should not be accepted. Give a proper message to the user and prompt again.
+    
+
+}
+
+
+
+
