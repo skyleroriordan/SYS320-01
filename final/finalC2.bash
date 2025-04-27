2629fa6940ca863e97e985d13102b95ee0f911a3
skyleroriordan <113953398+skyleroriordan@users.noreply.github.com>
2025-04-26 20:26:18 -0400

    Create finalC2.bash

---
+++ final/finalC2.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+ 
+ logfile="$1"
+ iocfile="$2"
+ outputfile="report.txt"
+ 
+ > "$outputfile"
+ 
+ while read -r ioc; do
+     grep "$ioc" "$logfile" | while read -r line; do
+ 	ip=$(echo "$line" | cut -d' ' -f1)
+ 	datetime=$(echo "$line" | cut -d' ' -f4 | tr -d '[]')
+ 	page=$(echo "$line" | cut -d' ' -f7)
+ 	echo "$ip $datetime $page"  >> "$outputfile"
+     done
+ done < "$iocfile"
